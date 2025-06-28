9c87e1d3b060fd90ae7103ecd2012212c9821677
Satwik <satwik_bh@hotmail.com>
2025-06-28 22:02:18 +0530

    Improving code and UTs using gemini

--- src/main/scala/LoadData.scala
+++ src/main/scala/LoadData.scala
@@ -1,9 +1,11 @@
 import MLAlgos.DimensionalityReduction
-import common.Helper.HelperClass
+import com.mongodb.spark.MongoSpark
+import com.mongodb.spark.config.ReadConfig
+import common.utils.DBUtilScala
 import org.apache.log4j.Logger
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.ml.feature.{HashingTF, IDF}
+import org.apache.spark.sql.SparkSession
 import services.Conversion.ConversionLogic
-import services.ReadData.ReadFromMongo
 
 import scala.collection.JavaConverters._
 
@@ -14,63 +16,42 @@ object LoadData extends App {
 
   val log: Logger = Logger.getLogger(LoadData.getClass)
 
-  def sparkInvocations: SparkContext = {
-    val conf: SparkConf = new SparkConf()
-      .setAppName("MLScala")
-      .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-    val sc: SparkContext = new SparkContext(conf)
-    sc
+  def sparkInvocations: SparkSession = {
+    val spark = SparkSession.builder()
+      .appName("MLScala")
+      .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+      .getOrCreate()
+    spark
   }
 
   def newStart(): Unit = {
     log.info("************ Program Started *************")
     val startTime = System.currentTimeMillis()
-    val sc = sparkInvocations
+    val spark = sparkInvocations
+    val sc = spark.sparkContext
 
-    val allMongoDocs = ReadFromMongo.getAllMongoDocs.asScala.map(line => {
-      (line._1, line._2.asScala.toArray)
-    }
-    ).toMap
+    val mongoRDD = DBUtilScala.mongoConnectorRDD(sc)
+    val documents = mongoRDD.map(doc => {
+      ConversionLogic.getDoc(doc.toJson).asScala.toArray
+    }).map(arr => arr.mkString(" "))
 
-    val arrayOfBOW = allMongoDocs.map(eachDocPair => {
-      val something = eachDocPair._2.map(eachDoc => {
-        val some = ConversionLogic.getDoc(eachDoc).asScala.toArray
-        some
-      })
-      something.flatten.distinct
-    }).toArray
+    val documentsDF = spark.createDataFrame(documents.map(Tuple1.apply)).toDF("text")
 
-    val sparseMatrix = HelperClass.getVector(sc, arrayOfBOW)
-    println(sparseMatrix.length)
+    val hashingTF = new HashingTF()
+      .setInputCol("text").setOutputCol("rawFeatures").setNumFeatures(1000)
 
-    val inputDataRDD = sc.parallelize(sparseMatrix)
-    val projectedVector = DimensionalityReduction.mllibPCA(inputDataRDD)
+    val featurizedData = hashingTF.transform(documentsDF)
+
+    val idf = new IDF().setInputCol("rawFeatures").setOutputCol("features")
+    val idfModel = idf.fit(featurizedData)
+
+    val rescaledData = idfModel.transform(featurizedData)
+
+    val projectedVector = DimensionalityReduction.mllibPCA(rescaledData.select("features").rdd.map(row => row.getAs[org.apache.spark.ml.linalg.SparseVector](0).asML))
 
     projectedVector.foreach(line => {
       println(line.toArray.mkString)
     })
-
-    /*
-      Breeze SVD Implementation
-     */
-
-
-    /* Spark MLLib KMeans clustering
-        val clusters = ClusteringAlgos.sparkMllibKmeans(inputDataRDD, 10)
-        log.info("Cluster Centers: ")
-        clusters.foreach(println)
-     */
-
-    /* Smile KMeans Clustering
-        val model = ClusteringAlgos.smileKMeans(input_data, 10)
-        println(model.mkString)
-     */
-
-    /* MrSqueeze LSH Implementation
-        val inputData = sc.parallelize(sparseMatrix)
-        val lshBasedClusters = LSHAlgos.mrSqueezeLSH(input_data = inputData, p = 65537, m = 1000, numRows = 1000, numBands = 100, minClusterSize = 2)
-        println(lshBasedClusters.saveAsTextFile("/home/satwik/Documents/Research/MLScalaMVN/lsh"))
-     */
 
     log.info("************ Program Ends *************")
     log.info(s"Time taken : ${System.currentTimeMillis() - startTime}")
